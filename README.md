7970dc01b00a6421a98dbf9256ca6aefad55d6f8
VinGuler <vinguler@gmail.com>
2021-05-03 13:04:42 +0300

    Added some comments and updated README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # nodejs-simple-server
-Simple Node.js server, using Express. Used for files serving and simple API
+Simple Node.js server, using Express. Used for files serving and simple API requests.
--- app.js
+++ app.js
@@ -6,13 +6,19 @@ const port = 3000
 const app = express()
 app.use(cors())
 
-app.use(express.static('./files'))
+// Change this to your static folder
+const staticFolder = './files'
 
-app.get('/get-file-names', (req, res) => {
-    const filenames = fs.readdirSync('./files')
+// Setting Express static folder
+app.use(express.static(staticFolder))
+
+// A simple get request to get a list of the static files names
+app.get('/get-static-file-names', (req, res) => {
+    const filenames = fs.readdirSync(staticFolder)
     res.json(filenames)
 })
 
+// Listening bro/sis
 app.listen(port, () => {
     console.log(`File server is now listening at http://localhost:${port}`)
 })
